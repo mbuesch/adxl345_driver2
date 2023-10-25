1531f08909b02cc7c4797b569b0637309779b6a5
Michael Buesch <m@bues.ch>
2023-10-25 19:18:52 +0200

    Add release script

---
+++ maintenance/makerelease.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+srcdir="$(realpath "$0" | xargs dirname)"
+srcdir="$srcdir/.."
+
+# Import the makerelease.lib
+# https://bues.ch/cgit/misc.git/tree/makerelease.lib
+die() { echo "$*"; exit 1; }
+for path in $(echo "$PATH" | tr ':' ' '); do
+	[ -f "$MAKERELEASE_LIB" ] && break
+	MAKERELEASE_LIB="$path/makerelease.lib"
+done
+[ -f "$MAKERELEASE_LIB" ] && . "$MAKERELEASE_LIB" || die "makerelease.lib not found."
+
+project=adxl345_driver2
+makerelease "$@"
